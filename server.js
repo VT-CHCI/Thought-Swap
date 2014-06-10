bc9ff5d485940e4ac3bc75d7c63265de9310e8d2
Michael Stewart <tgm@vt.edu>
2014-06-10 12:37:23 -0400

    changed OUR server (not the default one in grunt) to look at /app so that you don't need to worry about running grunt build

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ var io = require('socket.io')(http);
     
 // });
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/app'));
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
