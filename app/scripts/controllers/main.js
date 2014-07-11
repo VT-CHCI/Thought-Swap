644ce326d89638feefb05265ce9ee7a3f1dc2ef5
Adam Barnes <ajbarnes@vt.edu>
2014-07-11 15:39:16 -0400

    removed boilerplate items

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,6 @@
 'use strict';
 
 angular.module('thoughtSwapApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope, thoughtSocket) {
+    
   });
