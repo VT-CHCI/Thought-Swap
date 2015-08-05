73db0fbbe86e48894f355d0a98690f8cd7c19dce
Adam Barnes <ajbarnes@vt.edu>
2015-08-05 14:51:18 -0400

    tgm: stopping point before dndplaceholder styling goosehunt

--- app/facilitator/reciever.controller.js
+++ app/facilitator/reciever.controller.js
@@ -31,6 +31,17 @@
 
 		})();
 
+		// TEMP METHOD: Test thoughts - adds some thoughts automatically to the
+		// teacher view for ui testing.
+		$scope.testThoughts = function () {
+			console.log("got testThoughts cmd");
+			for (var i = 0; i < 16; i++) {
+				$scope.participantThoughts.push({
+					content: i + " Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+				});
+			}
+		};
+
 		$scope.animatedBounce = function (elemId) {
 			$animate.addClass($(elemId), 'animated bounce')
 				.then(function () {
